d9d757e18cb7a0daaa7a288ed27da844ea64509f
Galina Bobrik <galagrinko@gmail.com>
2024-12-10 11:55:20 +0400

    fix: response.ok added in fetchUserData

--- app/user/[userId]/page.tsx
+++ app/user/[userId]/page.tsx
@@ -1,44 +1,3 @@
-// import Link from "next/link";
-// import { User } from "../../interfaces/user";
-// import CustomUserIdCard from "@/components/CustomUserIdCard/CustomUserIdCard";
-
-// const fetchUserData = async (userId: number): Promise<User> => {
-//     const response = await fetch(
-//         `http://jsonplaceholder.typicode.com/users/${userId}`
-//     );
-//     if (!response.ok) {
-//         throw new Error("Failed to fetch user data");
-//     }
-//     const data: User = await response.json();
-//     return data;
-// };
-// interface PageProps {
-//     params: {
-//         userId: string;
-//     };
-//     searchParams?: { [key: string]: string | string[] | undefined };
-// }
-
-// const UserIdPage = async ({ params }: PageProps) => {
-//     const { userId } = params;
-//     const userIdNumber = parseInt(userId);
-//     const user: User = await fetchUserData(userIdNumber);
-
-//     return (
-//         <div className="flex flex-col justify-center items-center">
-//             <Link
-//                 href="/"
-//                 className="py-6 font-bold duration-300 transition-all hover:text-green-800 hover:scale-150"
-//             >
-//                 Home
-//             </Link>
-//             <CustomUserIdCard user={user} loading={false} />
-//         </div>
-//     );
-// };
-
-// export default UserIdPage;
-
 import Link from "next/link";
 import { User } from "../../interfaces/user";
 import CustomUserIdCard from "@/components/CustomUserIdCard/CustomUserIdCard";
@@ -47,34 +6,21 @@ const fetchUserData = async (userId: number): Promise<User> => {
     const response = await fetch(
         `http://jsonplaceholder.typicode.com/users/${userId}`
     );
+
     if (!response.ok) {
         throw new Error("Failed to fetch user data");
     }
+
     const data: User = await response.json();
     return data;
 };
 
-export async function generateMetadata({
+export default async function UserIdPage({
     params,
 }: {
-    params: { userId: string };
+    params: { userId: number };
 }) {
-    const userId = parseInt(params.userId);
-    const user = await fetchUserData(userId);
-    return {
-        title: user.name,
-        description: `User profile for ${user.name}`,
-    };
-}
-
-interface PageProps {
-    params: {
-        userId: string;
-    };
-}
-
-const UserIdPage = async ({ params }: PageProps): Promise<JSX.Element> => {
-    const userId = parseInt(params.userId);
+    const { userId } = await params;
     const user: User = await fetchUserData(userId);
 
     return (
@@ -88,6 +34,4 @@ const UserIdPage = async ({ params }: PageProps): Promise<JSX.Element> => {
             <CustomUserIdCard user={user} loading={false} />
         </div>
     );
-};
-
-export default UserIdPage;
+}
