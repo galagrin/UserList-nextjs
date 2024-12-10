abaa6d221cfd0f995bfd967aed8bbd8c01c26f0a
Galina Bobrik <galagrinko@gmail.com>
2024-12-10 11:32:50 +0400

    Prepare userIdPage

--- app/user/[userId]/page.tsx
+++ app/user/[userId]/page.tsx
@@ -1,3 +1,44 @@
+// import Link from "next/link";
+// import { User } from "../../interfaces/user";
+// import CustomUserIdCard from "@/components/CustomUserIdCard/CustomUserIdCard";
+
+// const fetchUserData = async (userId: number): Promise<User> => {
+//     const response = await fetch(
+//         `http://jsonplaceholder.typicode.com/users/${userId}`
+//     );
+//     if (!response.ok) {
+//         throw new Error("Failed to fetch user data");
+//     }
+//     const data: User = await response.json();
+//     return data;
+// };
+// interface PageProps {
+//     params: {
+//         userId: string;
+//     };
+//     searchParams?: { [key: string]: string | string[] | undefined };
+// }
+
+// const UserIdPage = async ({ params }: PageProps) => {
+//     const { userId } = params;
+//     const userIdNumber = parseInt(userId);
+//     const user: User = await fetchUserData(userIdNumber);
+
+//     return (
+//         <div className="flex flex-col justify-center items-center">
+//             <Link
+//                 href="/"
+//                 className="py-6 font-bold duration-300 transition-all hover:text-green-800 hover:scale-150"
+//             >
+//                 Home
+//             </Link>
+//             <CustomUserIdCard user={user} loading={false} />
+//         </div>
+//     );
+// };
+
+// export default UserIdPage;
+
 import Link from "next/link";
 import { User } from "../../interfaces/user";
 import CustomUserIdCard from "@/components/CustomUserIdCard/CustomUserIdCard";
@@ -12,17 +53,29 @@ const fetchUserData = async (userId: number): Promise<User> => {
     const data: User = await response.json();
     return data;
 };
+
+export async function generateMetadata({
+    params,
+}: {
+    params: { userId: string };
+}) {
+    const userId = parseInt(params.userId);
+    const user = await fetchUserData(userId);
+    return {
+        title: user.name,
+        description: `User profile for ${user.name}`,
+    };
+}
+
 interface PageProps {
     params: {
         userId: string;
     };
-    searchParams?: { [key: string]: string | string[] | undefined };
 }
 
-const UserIdPage = async ({ params }: PageProps) => {
-    const { userId } = await params;
-    const userIdNumber = parseInt(userId);
-    const user: User = await fetchUserData(userIdNumber);
+const UserIdPage = async ({ params }: PageProps): Promise<JSX.Element> => {
+    const userId = parseInt(params.userId);
+    const user: User = await fetchUserData(userId);
 
     return (
         <div className="flex flex-col justify-center items-center">
