1fc506e7736d554b34b58512db197a461ee36934
Galina Bobrik <galagrinko@gmail.com>
2024-12-10 11:01:42 +0400

    Fix: UserIdPage edited

--- app/user/[userId]/page.tsx
+++ app/user/[userId]/page.tsx
@@ -1,33 +1,67 @@
+// import Link from "next/link";
+// import { User } from "../../interfaces/user";
+
+// import CustomUserIdCard from "@/components/CustomUserIdCard/CustomUserIdCard";
+
+// const fetchUserData = async (userId: number) => {
+//     const response = await fetch(
+//         `http://jsonplaceholder.typicode.com/users/${userId}`
+//     );
+//     const data: User = await response.json();
+//     return data;
+// };
+
+// export default async function UserIdPage({
+//     params,
+// }: {
+//     params: { userId: string };
+// }) {
+//     const userId = parseInt(params.userId);
+//     const user: User = await fetchUserData(userId);
+
+//     return (
+//         <div className="flex flex-col justify-center items-center">
+//             <Link
+//                 href="/"
+//                 className="py-6 font-bold duration-300 transition-all  hover:text-green-800 hover:scale-150"
+//             >
+//                 Home
+//             </Link>
+//             <CustomUserIdCard user={user} loading={false} />
+//         </div>
+//     );
+// }
 import Link from "next/link";
 import { User } from "../../interfaces/user";
-
 import CustomUserIdCard from "@/components/CustomUserIdCard/CustomUserIdCard";
 
-const fetchUserData = async (userId: number) => {
+const fetchUserData = async (userId: number): Promise<User> => {
     const response = await fetch(
         `http://jsonplaceholder.typicode.com/users/${userId}`
     );
+    if (!response.ok) {
+        throw new Error("Failed to fetch user data");
+    }
     const data: User = await response.json();
     return data;
 };
 
-export default async function UserIdPage({
-    params,
-}: {
-    params: { userId: string };
-}) {
-    const userId = parseInt(params.userId);
-    const user: User = await fetchUserData(userId);
+const UserIdPage = async ({ params }: { params: { userId: string } }) => {
+    const { userId } = params;
+    const userIdNumber = parseInt(userId);
+    const user: User = await fetchUserData(userIdNumber);
 
     return (
         <div className="flex flex-col justify-center items-center">
             <Link
                 href="/"
-                className="py-6 font-bold duration-300 transition-all  hover:text-green-800 hover:scale-150"
+                className="py-6 font-bold duration-300 transition-all hover:text-green-800 hover:scale-150"
             >
                 Home
             </Link>
             <CustomUserIdCard user={user} loading={false} />
         </div>
     );
-}
+};
+
+export default UserIdPage;
